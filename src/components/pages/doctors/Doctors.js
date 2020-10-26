442e00ab2b68dd58e4d8a4c57ce8d2b97d2d7618
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-26 20:33:31 +0100

    add login and signup for doctors

--- src/components/pages/doctors/Doctors.js
+++ src/components/pages/doctors/Doctors.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-const Doctors = () => {
-    return (
-        <div>
-            Doctors page
-        </div>
-    )
-}
+const Doctors = ({ isVisible }) => {
+  return (
+    <div>
+      Doctors page
+      <motion.div animate={{ x: 150 }}>
+        dddddddddddddddddd
+        <br />
+        ddddddddddddddddddddd
+        <br />
+        ddddddddddddddddddddd
+      </motion.div>
+      {/* <motion.div
+        animate={{
+          x: 140,
+          y: 50,
+          scale: 1,
+          rotate: 0,
+        }}
+      >
+        lorem15dddddddddddddddddd
+      </motion.div> */}
+    </div>
+  );
+};
 
-export default Doctors
+export default Doctors;
+
+// import { motion } from "framer-motion"
+
+// export const MyComponent = ({ isVisible }) => (
+//     <motion.div animate={{ opacity: isVisible ? 1 : 0 }} />
+// )
