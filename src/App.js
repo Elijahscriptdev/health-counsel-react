b2e7083930ef48dbdc5c6999992530edb8e96de6
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-27 08:52:02 +0100

    add onsubmit functon to both login and signup forms

--- src/App.js
+++ src/App.js
@@ -31,3 +31,18 @@ function App() {
 }
 
 export default App;
+// "scripts": {
+  //   "dev": "react-scripts start",
+  //   "start": "serve -s build",
+  //   "build": "react-scripts build",
+  //   "test": "react-scripts test --env=jsdom",
+  //   "eject": "react-scripts eject",
+  //   "heroku-postbuild": "npm run build"
+  // }
+
+  // "scripts": {
+  //   "start": "react-scripts start",
+  //   "build": "react-scripts build",
+  //   "test": "react-scripts test",
+  //   "eject": "react-scripts eject"
+  // }
